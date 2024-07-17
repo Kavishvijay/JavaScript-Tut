5b1592a4861f9ec564783bc1468ebe454c8fb880
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-07-17 14:18:02 +0000

    Learned about dfferent type of conversions

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,36 @@
+//conversionOperations
+
+/*let score = 99;
+let score2 = "88"
+let x = "45asd"
+let y =  null
+
+console.log(typeof score);
+console.log(typeof (score2));
+
+intScore2 = Number(score2) // conversion from string to int   first word is always capital 'N' in number for conversion
+console.log(typeof (intScore2));
+
+intx = Number(x)
+console.log(typeof intx);
+console.log(intx);
+
+inty= Number(y)
+console.log(typeof inty);
+console.log(inty);
+
+//"Notes"
+//"33" = 33
+//"33ab" = NAN
+//null= 0
+// undefined = NAN
+//boolean true = 1: false = 0
+*/
+
+
+let login = "34" // true.....false in case of 0.... " "......
+isBoolean = Boolean(login)
+console.log(isBoolean);
+
+
+
