83bec6182fe90f724c94e395e2c494978b704c0f
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-07-17 13:47:26 +0000

    Learned about datatypes, alert, null

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version
+
+//alert("Hello")// error bcz we are using node not browser
+
+//mdn standard site
+//tc39
+
+
+
+//DataType
+//number
+//strig
+//boolean
+//null = Standalone value
+//undefined
+//symbol = unique
+
+console.log(typeof "kavish");
+console.log(typeof undefined); //o/p undefined
+console.log(typeof null);  //o/p object
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const empId = 049593
+let a = 40
+var c = 30
+Name = "kavish"
+let emp_year; 
+
+console.log(empId);
+console.table([empId, a, c,  Name, emp_year])
+
+/*   var was used earlier as it has issues with its Scope 
+now we only use const and let*/
