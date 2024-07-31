b84742392f03f659db88ea9acd8024b06faadb7d
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-07-31 07:37:20 +0000

    learned about Number datatype in js that is primitive and uses stack memory

---
+++ 01_basics/Number.js
@@ -0,0 +1,28 @@
+// let num = 500;
+// console.log(num);
+
+let num2 = new Number(144.5638);
+console.log(num2);
+
+console.log(num2.toString());
+
+console.log(num2.toString().includes(3));
+
+console.log(num2);
+
+console.log(num2.toFixed(3)); // works after the decimal
+console.log(num2.toPrecision(6)); // works on the whole value
+
+let a = num2.valueOf();
+console.log(a); // give 144.5678 same as num2 value;
+
+let num3 = new Number(100000000);
+console.log(num3.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
