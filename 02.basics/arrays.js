458a9ea0aa51dc33d307267e4b1db4eb29be62ac
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-07-31 18:15:05 +0000

    learned basics of array espically slice, splice, join

---
+++ 02.basics/arrays.js
@@ -0,0 +1,48 @@
+// let arr = [1,2,3,"hello", true];
+// console.log(arr);
+
+let arr2 = new Array(1,2,3,4,5);
+//console.log( arr2);
+
+// arr2.push(6);
+// arr2.push(7);
+// console.log( arr2);
+
+// arr2.pop();
+// console.log( arr2);
+
+// arr2.unshift(0)
+// console.log( arr2);
+
+// arr2.shift()
+// console.log( arr2);
+
+// console.log(arr2.includes(3));
+
+// console.log( arr2.indexOf(4));
+
+// let arr3 = arr2.join();
+// console.log( arr3);
+
+// console.log(typeof(arr2));
+// console.log( typeof arr3);
+
+// console.log(arr3.split('2'));
+
+// slice splice
+
+console.log("A ", arr2);
+console.log(arr2.slice(1,3) );
+console.log("B ", arr2);
+
+console.log(arr2.splice(1,3));
+console.log("C ", arr2 );
+
+
+
+
+
+
+
+
+
