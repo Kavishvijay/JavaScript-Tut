7bc1b2935f3ce0e996f0f8c0efa4bd84b9f29a66
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-08-01 17:50:11 +0000

    learned about objects

--- 02.basics/objects.js
+++ 02.basics/objects.js
@@ -7,12 +7,30 @@ let obj1 = {name : "kavish",
     [uid] : "007"
 }
 //types of accesing the object content...2nd one is important
-console.log(obj1.name);
-console.log(obj1.age);
-console.log(obj1['age']);
-console.log(obj1['rollno']);
-console.log(obj1.placementon);
-console.log( obj1[uid]);
-console.log(typeof obj1[uid]);
+// console.log(obj1.name);
+// console.log(obj1.age);
+// console.log(obj1['age']);
+// console.log(obj1['rollno']);
+// console.log(obj1.placementon);
+// console.log( obj1[uid]);
+// console.log(typeof obj1[uid]);
+
+obj1.rollno = "PCE21CR0007"
+//Object.freeze(obj1)
+obj1.rollno = "PCE21CR007"
+
+//console.log(obj1);
+
+obj1.lastname = "Vijay"
+
+
+ obj1.greetings = function(){console.log("Hello!boyee");}
+ obj1.greetingstwo = function(){console.log(`hey!! ${this.name} ${this.lastname}`);}
+ console.log(obj1.greetings);
+// console.log(obj1.greetings());
+// console.log(obj1.greetingstwo());
+
+//console.log(obj1);
+
 
 
