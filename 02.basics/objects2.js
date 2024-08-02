2545604b316f258f14d82036ded7220cceeff919
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-08-02 06:23:01 +0000

    objects

--- 02.basics/objects2.js
+++ 02.basics/objects2.js
@@ -4,10 +4,10 @@ let tinderuser = new Object();
 //adding values to singleton object 
 tinderuser.id = "kv12"
 tinderuser.isloggedin = false
-tinderuser.name = {userfullname :{
-name: "kavish",
-lastname: "vijay"
-}}
+//tinderuser.name = {userfullname :{
+//name: "kavish",
+//lastname: "vijay"
+//}}
 
 //console.log(tinderuser.name.userfullname?.lastname);
 
@@ -16,9 +16,23 @@ let obj1 = {1: "a", 2:"b"}
 let obj2 = {3: "a", 4:"b"}
 
 let obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 //joining two objects using assign function Objects.assign(target,source)
 let obj4 = Object.assign({}, obj1, obj2)
-console.log(obj4);
+//console.log(obj4);
+
+//objects into array and accesing
+let arr = [{1: "a", 2:"b"}, {3: "a", 4:"b"}]
+//console.log(arr[0][1]);
+
+//objests functions
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('name'));
+
+
+
 
