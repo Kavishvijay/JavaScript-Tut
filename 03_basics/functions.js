12441d513d0c1cb58b32637bf9c520460e34fba7
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-08-06 15:19:30 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,21 @@
+function sum(num1, num2){
+    let res = num1+num2
+    return res
+
+}
+
+sum(2,3)
+
+let res = sum(2, 3);
+console.log(res);
+
+//console.log does not return value
+
+
+function logedin(name){
+return `${name} just loged in`
+}
+
+console.log(logedin("kavish"));
+ 
+
