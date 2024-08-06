b16ed20e4708e69b5f5e1ac5deb409d7e523028c
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-08-06 15:43:27 +0000

    function

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -1,21 +1,52 @@
-function sum(num1, num2){
-    let res = num1+num2
-    return res
+// function sum(num1, num2){
+//     let res = num1+num2
+//     return res
 
-}
+// }
 
-sum(2,3)
+// sum(2,3)
 
-let res = sum(2, 3);
-console.log(res);
+// let res = sum(2, 3);
+// console.log(res);
 
-//console.log does not return value
+// //console.log does not return value
 
 
-function logedin(name){
-return `${name} just loged in`
-}
+// function logedin(name){
+// return `${name} just loged in`
+// }
 
-console.log(logedin("kavish"));
+// console.log(logedin("kavish"));
+
+// function logedin(name ){
+//     if(!name)
+//     {
+//         console.log("please enter name");
+//         return
+        
+//     }
+//      return `${name} just loged in`
+//      }
+    
+//      console.log(logedin("")); //undefined
+
+
+//we can also pass default value for parameters if argument is missing
+
+     function logedin(name= "ram" ){
+        if(!name)
+        {
+            console.log("please enter name");
+            return
+            
+        }
+         return `${name} just loged in`
+         }
+        
+         console.log(logedin()); 
+
+
+
+
  
 
