e007c06aa9b2b744732a398171bfd42952f76802
Kavish vijay <96161066+Kavishvijay@users.noreply.github.com>
2024-08-06 13:32:57 +0000

    learned about JSON

---
+++ 02.basics/json.js
@@ -0,0 +1,25 @@
+//destructuring for fetching data in json formate, earlier it was in xml formate
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
